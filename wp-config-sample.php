d89505b8bf692427192bd69e8b4890a1816e9897
ghtfva <110241171+ghtfva@users.noreply.github.com>
2022-08-01 16:02:02 +0800

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -18,24 +18,29 @@
  * @package WordPress
  */
 
-// ** Database settings - You can get this info from your web host ** //
+$_SERVER['HTTPS'] = 'on';
+
+// ** Heroku Postgres settings - from Heroku Environment ** //
+$db = parse_url($_ENV["DATABASE_URL"]);
+
+// ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define('DB_NAME', trim($db["path"],"/"));
 
-/** Database username */
-define( 'DB_USER', 'username_here' );
+/** MySQL database username */
+define('DB_USER', $db["user"]);
 
-/** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+/** MySQL database password */
+define('DB_PASSWORD', $db["pass"]);
 
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
+/** MySQL hostname */
+define('DB_HOST', $db["host"] . ':' . $db["port"]);
 
-/** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
 
-/** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
 
 /**#@+
  * Authentication unique keys and salts.
